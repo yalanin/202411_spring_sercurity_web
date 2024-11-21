42ae22ca7efc4047a29bb05d0ef121f85ed40023
Renee <Renee@gogofundinginc.com>
2024-11-21 10:35:51 +0800

    電影 api 測試

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,3 +6,16 @@ CREATE TABLE IF NOT EXISTS member
     name      VARCHAR(256) NOT NULL,
     age       INT          NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS role
+(
+    role_id   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS member_has_role
+(
+    member_has_role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    member_id          INT NOT NULL,
+    role_id            INT NOT NULL
+);
